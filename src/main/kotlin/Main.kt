db30cc965306b9b19a568ffc940496c29d7e3251
kroune <kr0ne@tuta.io>
2024-07-01 20:44:33 +0300

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,9 @@ package com.kroune
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.util.*
 
+@Deprecated("it will be replaced with purpose specific classes")
 @Serializable
 class NetworkResponse(
     @Suppress("unused") val code: Int,
@@ -14,14 +16,12 @@ class NetworkResponse(
     }
 }
 
-sealed class ApiException: Exception() {
-    class InvalidJwtToken: ApiException()
-    class InvalidCredentials: ApiException()
-    class IncorrectGameId: ApiException()
-    class InvalidMove: ApiException()
-    class IllegalMove: ApiException()
-    class BadLogin: ApiException()
-    class BadPassword: ApiException()
-    class LoginInUse: ApiException()
-    class Internal: ApiException()
+@Serializable
+class GameResponse(
+    val message: String,
+    val counter: Int
+) {
+    fun encode(): String {
+        return Json.encodeToString<GameResponse>(this@GameResponse)
+    }
 }
