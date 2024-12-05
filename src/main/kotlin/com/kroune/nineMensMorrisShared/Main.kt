d8439fe65f8d3ae27a86b4942dc101a63d59f6d7
kroune <123553746+kroune@users.noreply.github.com>
2024-12-05 23:11:22 +0300

    update

--- src/main/kotlin/com/kroune/nineMensMorrisShared/Main.kt
+++ src/main/kotlin/com/kroune/nineMensMorrisShared/Main.kt
@@ -1,32 +1,15 @@
 package com.kroune.nineMensMorrisShared
 
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-
-@Deprecated("it will be replaced with purpose specific classes")
-@Serializable
-class NetworkResponse(
-    @Suppress("unused") val code: Int,
-    @Suppress("unused") val message: String?
-) {
-    fun encode(): String {
-        return Json.encodeToString<NetworkResponse>(this@NetworkResponse)
-    }
-}
 
 @Serializable
-class GameResponse(
-    val message: String,
-    val counter: Int
-) {
-    fun encode(): String {
-        return Json.encodeToString<GameResponse>(this@GameResponse)
-    }
-}
-
-sealed class GameEndReason(val isFirstUser: Boolean) {
-    class UserGaveUp(isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
-    class UserWasTooSlow(isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
-    class Normal(isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
+sealed class GameEndReason(val isFirstUser: Boolean?) {
+    @Serializable
+    data class UserGaveUp(val isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
+    @Serializable
+    data class UserWasTooSlow(val isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
+    @Serializable
+    data class Normal(val isFirstUserWon: Boolean) : GameEndReason(isFirstUserWon)
+    @Serializable
+    class Aborted : GameEndReason(null)
 }
